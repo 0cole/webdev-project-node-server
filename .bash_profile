ccecd7c021f11a27c886fac5833649694d7ada17
Cole0609 <cah062003@gmail.com>
2023-12-06 15:56:06 -0500

    connected mongoose

---
+++ .bash_profile
@@ -0,0 +1,3 @@
+export DB_CONNECTION_STRING=mongodb+srv://user:supersecretpassword@cluster0.maihowa.mongodb.net/?retryWrites=true&w=majority
+export FRONTEND_URL=http://localhost:3000
+export NODE_ENV=development
--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 import express from "express";
+import session from "express-session";
 import cors from "cors";
 import "dotenv/config";
 import GlobalRoutes from "./messages/global/globalRoutes.js";
 import PrivateRoutes from "./messages/private/privateRoutes.js";
 import UserRoutes from "./users/userRoutes.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/project";
+mongoose.connect(
+	"mongodb+srv://user:supersecretpassword@cluster0.maihowa.mongodb.net/project?retryWrites=true&w=majority"
+);
 
 const app = express();
-app.use(cors());
+
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
+
+const sessionOptions = {
+	secret: "any string",
+	resave: false,
+	saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+	sessionOptions.proxy = true;
+	sessionOptions.cookie = {
+		sameSite: "none",
+		secure: true,
+	};
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
 GlobalRoutes(app);
 PrivateRoutes(app);
