26caa1ed21564858b42fbbfe8e9c9e6bdb373961
Cole0609 <cah062003@gmail.com>
2023-12-14 13:55:59 -0500

    fixed update

--- users/dao.js
+++ users/dao.js
@@ -5,9 +5,7 @@ export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) => model.findOne({ username: username });
 export const findUserByFavoriteArtist = (favoriteArtist) => model.findOne({ favoriteArtist: favoriteArtist });
 export const findUserByCredentials = (username, password) => model.findOne({ username, password });
-export const updateUser = (userId, user) => 
-{return 
-model.updateOne({ _id: userId }, { $set: user }); };
+export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
 export const changeEmail = async (userId, newEmail) => {
     try {
